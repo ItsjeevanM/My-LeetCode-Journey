0a664d1da9b63e3171ba2376697eec69ae937f57
Jeevan M <147178005+ItsjeevanM@users.noreply.github.com>
2026-07-16 00:07:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
+        int firstOd = n * n;
+        int firstEven = n *(n+1);
+        gcd(firstOd , firstEven);
+
         return n ;
-    }
+    } 
 };

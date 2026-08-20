7e2135dd65d6150ecce1fc6b2e9785bf805b54c5
Jeevan M <147178005+ItsjeevanM@users.noreply.github.com>
2026-08-20 22:03:32 +0530

    Time: 0 ms (100%), Space: 62.9 MB (49.44%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int first = 0, last = height.size() - 1;
-        int total = 0; // initialize total
+        int first = 0;
+        int last = height.size() - 1;
+        int total = 0;
 
-        while (first < last) {  // loop until pointers meet
-            int width = last - first;  // correct width
+        while(first < last){
+            int width = last - first;
             int h = min(height[first], height[last]);
             int currarea = width * h;
-            total = max(total, currarea);  // update max
 
-            // move the pointer pointing to the shorter line
-            if (height[first] < height[last]) {
+            total = max(currarea, total);
+
+            if(height[first]< height[last]){
                 first++;
-            } else {
+            }else{
                 last--;
             }
         }
-
         return total;
     }
 };

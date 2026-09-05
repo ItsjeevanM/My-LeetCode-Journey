2064bdc53a3ef04b8e8beeeb5ce92d5606db250c
Jeevan M <147178005+ItsjeevanM@users.noreply.github.com>
2026-09-05 23:41:15 +0530

    Time: 8 ms (70.04%), Space: 202.6 MB (85.02%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,20 +1,21 @@
-// max number in array - min number of array , if they are less than or equal to k , return the index;
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> stb(n);
-        stb[n-1] = nums[n-1];
-        for(int i = n-2;i>=0;i--){
-            stb[i]= min(stb[i+1], nums[i]);
-        } 
+        vector<int> stabilityscore(n);
+        stabilityscore[0] = nums[0];
 
-        int previousmax = 0;
-        for(int i = 0;i<n;i++){
-            previousmax = max(previousmax,nums[i]);
-            if(previousmax - stb[i] <= k)
-            return i;
+        for(int i = 1;i<n; i++){
+            stabilityscore[i] = max(nums[i] , stabilityscore[i - 1]);
         }
-        return -1;
-    }
+        int minNumber = INT_MAX;
+        for(int i = n -1;i>=0;i--){
+            minNumber = min(minNumber, nums[i]);
+            stabilityscore[i] -= minNumber;
+        }
+
+        for(int i = 0;i<n;i++)
+            if(stabilityscore[i] <= k) return i;
+            return -1;
+    }      
 };
